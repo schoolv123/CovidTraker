8ef086ebf869f7ffa9777a76ca51c56f1bc09359
schoolv123 <74983136+schoolv123@users.noreply.github.com>
2021-05-15 14:52:55 +0530

    content changes main card content

--- src/Covidtraker.jsx
+++ src/Covidtraker.jsx
@@ -21,28 +21,41 @@ const Covidtraker = () => {
         <h2> Live Covid 19 Case Count </h2>
         <div className="content-wrapper">
           <div className="card state">
-            <h3>India</h3>
+            <h3>
+              <span>country</span> India
+            </h3>
             <p>Total State Data</p>
           </div>
           <div className="card activity">
-            <h3>Total Active</h3>
+            <h3>
+              <span>Total</span> Active
+            </h3>
             <p>{data.active}</p>
           </div>
           <div className="card confirmed">
-            <h3>Total Confirmed</h3>
+            <h3>
+              <span>Total</span> Confirmed
+            </h3>
             <p>{data.confirmed}</p>
           </div>
 
           <div className="card recovery">
-            <h3>Total Recovery</h3>
+            <h3>
+              <span>Total</span> Recovery
+            </h3>
             <p>{data.recovered}</p>
           </div>
           <div className="card dathes">
-            <h3>Total Deaths</h3>
+            <h3>
+              <span>Total</span> Deaths
+            </h3>
             <p>{data.deaths}</p>
           </div>
           <div className="card time">
-            <h3>Last Updated Time</h3>
+            <h3>
+              {" "}
+              <span>Last</span> Updated Time
+            </h3>
             <p>{data.lastupdatedtime}</p>
           </div>
         </div>
