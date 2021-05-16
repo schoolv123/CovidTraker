b7d59c747b2a71f8bf5b052924c3056b93bebd4c
schoolv123 <74983136+schoolv123@users.noreply.github.com>
2021-05-16 10:08:03 +0530

    New file created as new component

---
+++ src/RouteExample.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const RouterExample = () =>{
+
+return (
+<>
+
+</>
+);
+}
+export default RouterExample;
